76cad0ba4a7a793fef28182594f759d6a1cfe576
willis-tan <wtan956@gmail.com>
2023-01-24 21:50:45 -0800

    data ingestion to local repo

--- src/tv_details.py
+++ src/tv_details.py
@@ -1,12 +1,5 @@
 """
-Trending shows are sorted from most trendy to least trendy.
-In other words, page 1 results are more trendy than page 20 results.
 
-Each page stores 20 results.
-Max pages = 1000
-
-For simplicity's sake, fetch the top 500 trending shows each week.
-Which means 25 pages need to be requested.
 """
 
 import json
@@ -18,8 +11,10 @@ from datetime import datetime
 
 api_key = os.environ.get('TMDB_API_KEY')
 
-uri = 'https://api.themoviedb.org/3/trending/tv/week'
+uri = 'https://api.themoviedb.org/3/tv/{tv_id}'
 auth_uri = uri + '?api_key={}'.format(api_key)
+full_url = auth_uri + \
+    '&append_to_response={}'.format('watch/providers,content_ratings')
 headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
@@ -32,22 +27,24 @@ s.headers.update(headers)
 # Function that returns results for a page
 # In order to speed up the ingestion process, this function will used in
 # multi-threaded calls.
-def func(session, auth_uri,  page_num):
-    full_url = auth_uri + '&page={}'.format(page_num)
-    return session.request('GET', full_url).json()['results']
 
 
-# first 25 pages == top 500 trending shows
-page_numbers = range(1, 26)
+def func(session, uri, tv_id,):
+    final_url = uri.format(tv_id=tv_id)
+    return session.request('GET', final_url).json()
+
+
+# run SELECT DISTINCT "tv_id" FROM fact_table
+tv_ids = [1399]
 # Use a with statement to ensure threads are cleaned up promptly
 with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
     # Although TMDb has no rate limits, this is merely a personal project,
     # therefore the number of workers will be capped at 8
-    page_result = executor.map(
+    show_results = executor.map(
         func,
         repeat(s),
-        repeat(auth_uri),
-        page_numbers
+        repeat(full_url),
+        tv_ids
     )
 
 # create a dictionary to store all 25 pages in the "results" list
@@ -56,13 +53,7 @@ now = datetime.now()
 ingest_date = now.strftime("%Y-%m-%d")
 
 # finally, dump the formatted contents in JSON file
-with open('{}_trending-shows.json'.format(ingest_date), 'w') as f:
-    rank = 1
-    for page in page_result:
-        for show in page:
-            # for each result, assign its rank
-            show['rank'] = rank
-
-            f.write(json.dumps(show))
-            f.write('\n')
-            rank += 1
+with open('details.json', 'w') as f:
+    for show in show_results:
+        show_details['results'].append(show)
+        f.write(json.dumps(show_details, indent=2))
